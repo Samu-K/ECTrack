6514df1e6b33f2f9edfa8cc3dcf9dffdacce50ed
Samu <samu.kaarlela@gmail.com>
2024-09-03 16:12:26 +0300

    Added in checkstyle

--- src/main/java/fi/tuni/App.java
+++ src/main/java/fi/tuni/App.java
@@ -1,13 +1,11 @@
 package fi.tuni;
 
 /**
- * Hello world!
+ * Hello world.
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
 }
